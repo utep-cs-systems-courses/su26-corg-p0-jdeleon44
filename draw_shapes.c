611c8c0d09ccd03e1da87a44e808ea53e00ca5c1
jdeleon44 <jdeleon4@miners.utep.edu>
2026-06-19 12:06:55 -0600

    Update draw_shapes.c

--- draw_shapes.c
+++ draw_shapes.c
@@ -25,24 +25,3 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
-
-// Prints an arrow pointing right with specified size
-void print_arrow(int leftCol, int size)
-{
-  // Arrow head (upper half of triangle)
-  for (int row = 0; row < size; row++) {
-    int col;
-    // Print leading spaces
-    for (col = 0; col < leftCol + size - row; col++) putchar(' ');
-    // Print arrow head
-    for (       ; col < leftCol + size + row + 1; col++) putchar('*');
-    putchar('\n');
-  }
-  // Arrow shaft (vertical line)
-  for (int row = 0; row < size; row++) {
-    int col;
-    for (col = 0; col < leftCol + size; col++) putchar(' ');
-    putchar('*');
-    putchar('\n');
-  }
-}
