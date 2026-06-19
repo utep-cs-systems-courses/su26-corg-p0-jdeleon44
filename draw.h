2b0fa7f81f3cda9241a2149c5ab2c59079f24262
jdeleon44 <jdeleon4@miners.utep.edu>
2026-06-19 11:40:40 -0600

    Update draw.h

--- draw.h
+++ draw.h
@@ -4,6 +4,7 @@
 void print_triangle(int startCol, int size);
 void print_square(int startCol, int size);
 void print_char_5x7(char c);
+void print_arrow(int startCol, int size);
 
 extern const unsigned char font_5x7[][5];
 
