799686573fc04613f0a878bc7bd804a1169a78e8
jdeleon44 <jdeleon4@miners.utep.edu>
2026-06-22 16:11:17 -0600

    Draw center vertical line in triangle (replace center '*' with '|')

--- draw_shapes.c
+++ draw_shapes.c
@@ -15,13 +15,18 @@ void print_square(int leftCol, int size)
 }
 
 // Prints a triangle of specified height whose left edge is at col leftCol.
+// Draws a vertical line down the center column of the triangle using '|'.
 void print_triangle(int leftCol, int size)
 {
+  int centerCol = leftCol + size;
   for (int row = 0; row <= size; row++) {
     int minCol = leftCol + size - row, maxCol = leftCol + size + row;
     int col;
     for (col = 0; col < minCol; col++) putchar(' ');
-    for (       ; col <= maxCol; col++) putchar('*');
+    for (       ; col <= maxCol; col++) {
+      if (col == centerCol) putchar('|');
+      else putchar('*');
+    }
     putchar('\n');
   }
 }
