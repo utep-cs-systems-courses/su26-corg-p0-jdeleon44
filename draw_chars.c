b2b58c32fe7c25d97cc863c500b400a0296ab83f
jdeleon44 <jdeleon4@miners.utep.edu>
2026-06-22 17:01:59 -0600

    Add function to print 8x12 character bitmap

--- draw_chars.c
+++ draw_chars.c
@@ -14,3 +14,16 @@ void print_char_5x7(char c)
   }
   putchar('\n');
 }
+void print_char_8x12(char c)
+{
+  c -= 0x20;
+  for (char row = 0; row < 8; row++) {
+    unsigned short rowBits = font_8x12[c][row];
+    for (char col = 0; col < 12; col++) {
+      unsigned short colMask = 1 << (7-col);
+      putchar( (rowBits & colMask) ? '*' : ' ');
+    }
+    putchar('\n');
+  }
+  putchar('\n');
+}
