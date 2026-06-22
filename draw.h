68679420e477d28da115a3a8949c0333d4beb7e1
jdeleon44 <jdeleon4@miners.utep.edu>
2026-06-22 17:03:01 -0600

    Add 8x12 character font and print function

--- draw.h
+++ draw.h
@@ -5,7 +5,9 @@ void print_triangle(int startCol, int size);
 void print_square(int startCol, int size);
 void print_char_5x7(char c);
 void print_arrow(int startCol, int size);
+void print_char_8x12(char c);
 
 extern const unsigned char font_5x7[][5];
+extern const unsigned char font_8x12[95][12];
 
 #endif
